7a9018932ea0367cc3006f70134a78fa5f5858d7
aswego123 <77005833+aswego123@users.noreply.github.com>
2021-01-09 20:31:46 +0530

    new changes

--- chess_engine.js
+++ chess_engine.js
@@ -1,7 +1,7 @@
 var board = null
 var newGame = new Chess();
 
-/*
+
 function onDragStart (source, piece, position, orientation) 
 {
     if(newGame.game_over())
@@ -19,22 +19,22 @@ function onDragStart (source, piece, position, orientation)
 
 function makeRandomMoves()
 {
-    var possibleMovements = newGame.moves()
-    if(possibleMovements.length()==0)
+    var possibleMoves = newGame.moves()
+    if(possibleMoves.length()==0)
     {
         return
     }
     var randomIndex = Math.floor(Math.random() * possibleMoves.length)
-    newGame.move(possibleMovements[randomIndex])
+    newGame.move(possibleMoves[randomIndex])
     board.position(newGame.fen())
 
 }
 
-function moveFromSource(source, destination)
+function onDrop(source, target)
 {
-    var newMove = newGame.move({
-        to: destination,
+    var move = newGame.move({
         from: source,
+        to: target,
         promotion: 'q'
     })
 
@@ -42,11 +42,11 @@ function moveFromSource(source, destination)
     {
         return 'snapback' //illegal move_comes back on its own
     }
-    window.setTimeout(randomMoves, 250)
+    window.setTimeout(makeRandomMove, 250)
 
 }
 
-function updateBoard(){
+function onSnapEnd(){
     board.position(newGame.fen())
 }
 
@@ -54,56 +54,9 @@ var config = {
     draggable: true,
     position: 'start',
     onDragStart: onStart,
-    onDragMove: moveFromSource,
-    onSnapEnd: updateBoard
+    onDrag: onDrag,
+    onSnapEnd: onSnapEnd
 }
 
-board = Chessboard(myBoard, config)
+board = Chessboard('myBoard', config)
 
-*/
-
-
-function onDragStart (source, piece, position, orientation) {
-    if (newGame.game_over())
-    {
-      return false
-    } 
-  
-    if (piece.search(/^b/) !== -1)
-    { 
-      return false
-    }
-  }
-  
-  function makeRandomMove () {
-    var possibleMoves = newGame.moves()
-  
-    if (possibleMoves.length === 0) return
-  
-    var randomIndex = Math.floor(Math.random() * possibleMoves.length)
-    newGame.move(possibleMoves[randomIndex])
-    board.position(newGame.fen())
-  }
-  
-  function onDrop (source, target) {
-    var move = newGame.move({
-      from: source,
-      to: target,
-      promotion: 'q'
-    })
-    if (move === null) return 'snapback'
-    window.setTimeout(makeRandomMove, 250)
-  }
-  
-  function onSnapEnd () {
-    board.position(newGame.fen())
-  }
-  
-  var config = {
-    draggable: true,
-    position: 'start',
-    onDragStart: onDragStart,
-    onDrop: onDrop,
-    onSnapEnd: onSnapEnd
-  }
-  board = Chessboard('myBoard', config)
